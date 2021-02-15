6dfe495638e834e138d9d2b9671f7b4a35bc6940
Marco Aguilera <maaguilera@csumb.edu>
2021-02-15 15:48:11 -0800

    Different attempt at svg loading

--- public/src/img-ajax.js
+++ public/src/img-ajax.js
@@ -3,26 +3,22 @@ var foundAll = false;
 var id = $("#projectPortId").val();
 var svg = document.getElementById("loading_svg");
 
-async function showIcon(svg) {
-    await gsap.to(svg, {duration: 0.3, opacity: 1});
-}
-
-async function hideIcon(svg) {
-    await gsap.to(svg, {duration: 0.3, opacity: 0});
-}
-
 window.onscroll = function(ev) {
     if (((window.innerHeight + window.scrollY) >= document.body.scrollHeight) && foundAll == false) {
         // you're at the bottom of the page
         var url_imgs = '/more_imgs/' + id + "/"+ page;
         
-        showIcon(svg);
-
         $.ajax( {
             url: url_imgs,
             type: "POST",
             contentType: 'application/json',
             async: false,
+            beforeSend: function() {
+                $(svg).show(0);
+            },
+            complete: function() {
+                $(svg).hide(0);
+            },
             success: function(res) {
                 if (res['html'] == null) {
                     foundAll = true;
@@ -32,11 +28,8 @@ window.onscroll = function(ev) {
                     div.innerHTML += res['html'];
                     page = page + 1;
                 }
-
-                hideIcon(svg);
             },
             error: function() {
-                hideIcon(svg);
                 alert("Ajax not successful");
             }
         });

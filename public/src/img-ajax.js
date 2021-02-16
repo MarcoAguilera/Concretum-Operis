db3b6a90746f124977d518c9a46cc07a62a935f1
Marco Aguilera <maaguilera@csumb.edu>
2021-02-15 20:22:36 -0800

    Created loading animation

--- public/src/img-ajax.js
+++ public/src/img-ajax.js
@@ -1,24 +1,21 @@
 var page = 2;
 var foundAll = false;
+var ajaxInProg = false;
 var id = $("#projectPortId").val();
 var svg = document.getElementById("loading_svg");
 
 window.onscroll = function(ev) {
-    if (((window.innerHeight + window.scrollY) >= document.body.scrollHeight) && foundAll == false) {
-        // you're at the bottom of the page
+    if (((window.innerHeight + window.scrollY) >= document.body.scrollHeight) && foundAll == false && ajaxInProg == false) {
+        ajaxInProg = true;
         var url_imgs = '/more_imgs/' + id + "/"+ page;
         
+        gsap.to(svg, {duration: .5, opacity: 1});
+    
         $.ajax( {
             url: url_imgs,
             type: "POST",
             contentType: 'application/json',
-            async: false,
-            beforeSend: function() {
-                $(svg).show(0);
-            },
-            complete: function() {
-                $(svg).hide(0);
-            },
+            async: true,
             success: function(res) {
                 if (res['html'] == null) {
                     foundAll = true;
@@ -28,6 +25,10 @@ window.onscroll = function(ev) {
                     div.innerHTML += res['html'];
                     page = page + 1;
                 }
+            },
+            complete: function() {
+                ajaxInProg = false;
+                gsap.to(svg, {duration: .5, opacity: 0});
             },
             error: function() {
                 alert("Ajax not successful");
